76eb66468c47f7d834e8d21558f1b6355fe96bb3
Anastasiia Herusova <nastia.herusova@gmail.com>
2023-03-12 16:59:17 +0200

    task-4

--- src/utils/dataFormatter.js
+++ src/utils/dataFormatter.js
@@ -1,7 +1,10 @@
-import { formatDistanceToNow } from 'date-fns'
+import { formatDistanceToNow, format } from 'date-fns'
 
 
 export function dateFormatter(date) {
     return formatDistanceToNow(new Date(date), {addSuffix: true})
 }
 
+export function formatDateTransactions(date) {
+    return format(new Date(date), 'Pp')
+}
